a71bd5884915bfdde1cd68a8886f07df4ce53cad
kennethsy10 <sykennethangelo@gmail.com>
2024-09-28 11:11:06 +0800

    lesson2

---
+++ lesson2task.py
@@ -0,0 +1,18 @@
+age = [-12,13,-14,15,-16]
+
+for data in age:
+    if data == (-12,-14,-16):
+        print("negative : -12,-14,-16")
+    elif data == (13,15):
+        print("positive : 13,15")
+    break
+
+
+username = ["juan", "pedro", "james", "sagaad", "fuego"]
+
+for datauser in username:
+    if datauser == "james":
+        print("james cute")
+    else:
+        print("james none")
+        break
